1fef2cf481b2a937e10cb4bb72a64862b7b11b39
jake <jake@dropio.com>
2008-08-11 15:44:56 -0500

    Finished up the basic lottery example. Moving on

--- lottery/lottery.m
+++ lottery/lottery.m
@@ -3,8 +3,19 @@
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
-    [pool drain];
+	NSMutableArray *array;
+	array = [[NSMutableArray alloc] init];
+	int i;
+	for (i = 0; i < 10; i++) {
+		NSNumber *newNumber = [[NSNumber alloc] initWithInt:(i * 3)];
+		[array addObject:newNumber];
+	}
+	
+	for (i = 0; i < 10; i++) {
+		NSNumber *numberToPrint = [array objectAtIndex:i];
+		NSLog(@"The number at index %d is %@", i, numberToPrint);
+	}
+	
+	[pool drain];
     return 0;
 }
