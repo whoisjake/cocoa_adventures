30ee6d8f819335dc13b5c3ea77be5e4543b45aed
jake <jake@dropio.com>
2008-08-17 11:23:38 -0500

    Finished Chapter 8 with some Undo of adding, removing, and editing

--- RaiseMan/MyDocument.h
+++ RaiseMan/MyDocument.h
@@ -8,12 +8,20 @@
 
 
 #import <Cocoa/Cocoa.h>
+@class Person;
 
 @interface MyDocument : NSDocument
 {
 	NSMutableArray *employees;
+	IBOutlet NSTableView *tableView;
+	IBOutlet NSArrayController *employeeController;
 }
 
+- (IBAction) createEmployee:(id)sender;
 - (void) setEmployees:(NSMutableArray *)a;
+- (void) removeObjectFromEmployeesAtIndex:(int)index;
+- (void) insertObject:(Person *)p inEmployeesAtIndex:(int)index;
+- (void) startObservingPerson:(Person *)p;
+- (void) stopObservingPerson:(Person *)p;
 
 @end
--- RaiseMan/MyDocument.m
+++ RaiseMan/MyDocument.m
@@ -7,6 +7,7 @@
 //
 
 #import "MyDocument.h"
+#import "Person.h"
 
 @implementation MyDocument
 
@@ -30,9 +31,124 @@
 	if (a == employees)
 		return;
 	
+	for (Person *person in employees)
+		[self stopObservingPerson:person];
+	
 	[a retain];
 	[employees release];
 	employees = a;
+	
+	for (Person *person in employees)
+		[self startObservingPerson:person];
+}
+
+- (void) insertObject:(Person *)p inEmployeesAtIndex:(int)index
+{
+	NSLog(@"adding %@ to %@", p, employees);
+	// Add the inverse of this operation to the undo stack
+	NSUndoManager *undo = [self undoManager];
+	[[undo prepareWithInvocationTarget:self] removeObjectFromEmployeesAtIndex:index];
+	
+	if (![undo isUndoing])
+		[undo setActionName:@"Insert Person"];
+	
+	// Add the person to the array
+	[self startObservingPerson:p];
+	[employees insertObject:p atIndex:index];
+}
+
+- (void) removeObjectFromEmployeesAtIndex:(int)index
+{
+	Person *p = [employees objectAtIndex:index];
+	NSLog(@"removing %@ from %@", p, employees);
+	// Add the inverse of this operation to the undo stack
+	NSUndoManager *undo = [self undoManager];
+	[[undo prepareWithInvocationTarget:self] insertObject:p	inEmployeesAtIndex:index];
+	
+	if (![undo isUndoing])
+		[undo setActionName:@"Delete Person"];
+	
+	[self stopObservingPerson:p];
+	[employees removeObjectAtIndex:index];
+}
+
+- (void) stopObservingPerson:(Person *) person
+{
+	[person removeObserver:self	forKeyPath:@"personName"];
+	[person removeObserver:self forKeyPath:@"expectedRaise"];
+}
+
+- (void) startObservingPerson:(Person *) person
+{
+	[person addObserver:self forKeyPath:@"personName" options:NSKeyValueObservingOptionOld context:NULL];
+	[person addObserver:self forKeyPath:@"expectedRaise" options:NSKeyValueObservingOptionOld context:NULL];
+}
+
+- (void) changeKeyPath:(NSString *)keyPath
+			  ofObject:(id)obj
+			   toValue:(id)newValue
+{
+	// setValue:forKeyPath: will cause the key-value observing method
+	// to be called. which takes care of the undo stuff
+	[obj setValue:newValue forKeyPath:keyPath];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+					  ofObject:(id)obj
+						change:(NSDictionary *)change
+					   context:(void *)context
+{
+	NSUndoManager *undo = [self undoManager];
+	id oldValue = [change objectForKey:NSKeyValueChangeOldKey];
+	
+	if (oldValue == [NSNull null]) {
+		oldValue = nil;
+	}
+	
+	NSLog(@"oldValue = %@", oldValue);
+	
+	[[undo prepareWithInvocationTarget:self] changeKeyPath:keyPath ofObject:obj toValue:oldValue];
+	[undo setActionName:@"Edit"];
+}
+
+- (BOOL)endEditing
+{
+	NSWindow *w = [tableView window];
+	BOOL editingEnded = [w makeFirstResponder:w];
+	if (!editingEnded) {
+		return NO;
+	}
+	NSUndoManager *undo = [self undoManager];
+	if ([undo groupingLevel]) {
+		[undo endUndoGrouping];
+		[undo beginUndoGrouping];
+	}	
+	return YES;
+}
+
+
+- (IBAction)createEmployee:(id)sender
+{
+	// If a field is being edited, end editing
+	if ([self endEditing] == NO) {
+		return;
+	}
+	
+	Person *p = [employeeController newObject];
+	[employeeController addObject:p];
+	
+	// In case the user has sorted the content array
+	[employeeController rearrangeObjects];
+	
+	// Find the row of the new object
+	NSArray *a = [employeeController arrangedObjects];
+	int row = [a indexOfObjectIdenticalTo:p];
+	
+	// Start editing in the first column
+	[tableView editColumn:0
+					  row:row
+				withEvent:nil
+				   select:YES];
 }
 
 - (NSString *)windowNibName
